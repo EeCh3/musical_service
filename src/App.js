ba31f1c9f9b287c6f2c802455374a729c922601a
EeCh3 <dirgeinferno42@gmail.com>
2023-05-25 18:37:42 +0300

    поехала верстка

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import Sidebar from "./sidebar";
 import MusicPlayer from "./musicPlayer";
 
 
-export const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     -webkit-box-sizing: border-box;
     box-sizing: border-box;
-    font-family: 'Stratos-Regular';
-    font-style: normal;
-    font-weight: 400;
-    src: local('Stratos-Regular'),
-      url("../fonts/Stratos-Regular.woff2") format('woff2'),
+
+    @font-face {
+      font-family: 'Stratos-Regular';
+      font-style: normal;
+      font-weight: 400;
+      src: local('Stratos-Regular'),
+          url("../fonts/Stratos-Regular.woff2") format('woff2'),
+    }
 
     width: 100%;
     height: 100%;
